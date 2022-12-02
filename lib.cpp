32458b0038069f18e53353c55c361992a866fcdf
francescorighetti21 <117826738+francescorighetti21@users.noreply.github.com>
2022-12-02 20:28:06 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,12 @@
+#include "lib.h"
 
+bool convert(char &a){
+    if(a >= 65 && a <= 90){
+        a += 32;
+        return true;
+    }else if(a >= 97 && a <= 122){
+        a -= 32;
+        return true;
+    }
+    return false;
+}
